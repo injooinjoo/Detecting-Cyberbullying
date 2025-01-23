5ea3bbb844a445c57aad02c136596e8ae489c08d
injooinjoo <75089362+injooinjoo@users.noreply.github.com>
2025-01-23 15:55:33 +0900

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,14 @@
 
 import joblib
 
+# 모델 저장 함수
 def save_model(model, path):
     joblib.dump(model, path)
 
+# 모델 불러오기 함수
 def load_model(path):
     return joblib.load(path)
+
+# 평가 지표 저장 함수
+def save_metrics(metrics, path):
+    metrics.to_csv(path, index=False)
